4e0101f3cd71b1ceb19f7c14a9d5ea2986d9dc6d
Howard <howardjer@gmail.com>
2021-01-30 15:33:00 -0700

    found multiples of 3 or 5 AND 3 and 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
-for (i) in range(1, 11):
-    if i% 3 == 0:
+for (i) in range(1, 31):
+    if i % 3 == 0 and i % 5 == 0:
+        print ( str(i) + ' is multiple of 3 and 5')
+    elif i % 3 == 0:
         print ( str(i) + ' is multiple of 3')
-    elif i% 5 == 0:
+    elif i % 5 == 0:
         print ( str(i) + ' is multiple of 5')
     else:
         print ( str (i) )
