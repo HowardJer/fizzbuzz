b6ec6a1756844035e8cc15995e4a335294bebe6c
Howard <howardjer@gmail.com>
2021-01-30 15:26:57 -0700

    checking the multiples

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,8 @@
-for i in range(5):
-    print("hello world")
+for (i) in range(1, 11):
+    if i% 3 == 0:
+        print ( str(i) + ' is multiple of 3')
+    elif i% 5 == 0:
+        print ( str(i) + ' is multiple of 5')
+    else:
+        print ( str (i) )
+    
